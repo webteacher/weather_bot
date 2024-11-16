309a4620e570fb1eb3721f6e53579c490ad2d18b
Volodymyr <volodymyr.logika@gmail.com>
2024-11-16 21:23:46 +0200

    +

--- bot.py
+++ bot.py
@@ -1,48 +1,68 @@
 from telegram import KeyboardButton, ReplyKeyboardMarkup, Update
 import requests
 from telegram.ext import ApplicationBuilder, CommandHandler,  MessageHandler, filters
-# import settings
+import settings
 
-WEATHER_API_KEY= "39a56c1e858c31d721c7107d7d31e0df"
+WEATHER_API_KEY = "39a56c1e858c31d721c7107d7d31e0df"
 LOCATION = "https://maps.app.goo.gl/eJBDKhz1NGcJTtb96"
-keyboard = [[KeyboardButton("Наіслати геолокацію", request_location= True),
-             
+keyboard = [[KeyboardButton("Надіслати геолокацію", request_location=True),
+
              ]]
 
-# Функція для обробки команди /start
-async def start(update: Update, context):
-    
-    reply = ReplyKeyboardMarkup (keyboard=keyboard)
-    await update.message.reply_text("Привіт, я чат бот погоди! Надішли свою геолокацію", reply_markup=reply)
-    
 
-
-async def get_location(update,context):
-    location = update.message.location
-    lat,lon  = location.latitude, location.longitude
-    context.user_data[WEATHER_API_KEY] = (lat, lon)
-    await update.message.reply_text(f'Ваші координати:{lat,lon}')
-
-async   def get_weather(lat, lon, WEATHER_API_KEY):
+def get_weather(lat, lon, WEATHER_API_KEY):
+    """отримуємо поточну погоду від openweathermap
+    """
     url = (f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&units=metric&lang=uk")
     response = requests.get(url)
-    await update.message.reply_text(f'Погда у вашому регіоні::{lat,lon}')
     return response.json()
+
+def get_forecast(lat, lon, WEATHER_API_KEY):
+    """отримуємо прогноз погоди від openweathermap
+    """
+    url = (f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&units=metric&lang=uk&cnt=3")
+    response = requests.get(url)
+    return response.json()
+
+async def start(update: Update, context):
+    """Функція для обробки команди /start
+    """
+    reply = ReplyKeyboardMarkup(keyboard=keyboard)
+    await update.message.reply_text("Привіт, я чат бот погоди! Надішли свою геолокацію", reply_markup=reply)
+
+
+async def get_location(update, context):
+    location = update.message.location
+    lat, lon = location.latitude, location.longitude
+    context.user_data['location'] = (lat, lon)
+    weather_info = get_weather(lat,lon,WEATHER_API_KEY)
+    temp = weather_info['main']["temp"]
+    description = weather_info["weather"][0]["description"]
+    wind_speed = weather_info["wind"]["speed"]
+
+    keyboard2 = [
+        [KeyboardButton("Отримати прогноз")],
+        [KeyboardButton("Надіслати геолокацію", request_location=True)]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard2, resize_keyboard=True, one_time_keyboard=True)
+
+    await update.message.reply_text(f"Weather: {temp} °C, {description}, Швидкість вітру:{wind_speed} м/с", reply_markup = reply_markup)
+
+async def send_forecast(update,context):
+    lat, lon = context.user_data["location"]
+    forecast_info = get_forecast(lat,lon,WEATHER_API_KEY)
+    await update.message.reply_text(f"weather forecast {forecast_info}")
     
 
 
-
-
-    
 # Основна частина програми
 if __name__ == '__main__':
-    application = ApplicationBuilder().token("7347148528:AAF7UELcx96pl-5NIqHId3-mNWOP0GAMbs8").build()
+    application = ApplicationBuilder().token(settings.TOKEN).build()
 
     # Додаємо обробник для команди /start
     application.add_handler(CommandHandler('start', start))
     application.add_handler(MessageHandler(filters.LOCATION, get_location))
-    application.add_handler(MessageHandler(filters.LOCATION, get_weather))
-
+    application.add_handler(MessageHandler(filters.TEXT,send_forecast))
 
     # Запускаємо бота
     application.run_polling()
